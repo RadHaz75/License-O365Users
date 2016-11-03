014c673f7006a17be10df5fcfdc6682846fb381c
RadHaz75 <RadHaz75@gmail.com>
2016-11-03 13:49:21 -0400

    Update License-O365Users.ps1

--- License-O365Users.ps1
+++ License-O365Users.ps1
@@ -2,7 +2,7 @@
     .SYNOPSIS
      Updates Office 365 licenses of users from a CSV file
 	.DESCRIPTION
-	 This script will set or update the UsageLocation and licenses assigned to the users in the specified CSV file. If the CSV does not already exist, the 
+	 This script will set or update the Office 365 UsageLocation and licenses assigned to the users in the specified CSV file. If the CSV does not already exist, the 
 	 script can create it with the current set of licenses using the GenerateCSVFile parameter. To add a feature of a license to a user, put a 1 in the field. To 
 	 add all features of a license to a user, put a 1 in all fields for the license. To remove a feature of a license from a user, put a 0 in the field. To remove all 
 	 features of a license from a user, put a 0 in all fields for the license. To not modify a feature, license or UsageLocation for a user, leave the field blank.
